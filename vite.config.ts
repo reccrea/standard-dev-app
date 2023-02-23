01ece5d3119d411815eaa7b0f65b205065ccff21
reccrea_ <wellmoon0@163.com>
2023-02-23 12:48:01 +0800

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslintPlugin from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [vue(), eslintPlugin()],
+	server: {
+		host: 'localhost',
+		port: 9090,
+		open: true,
+	},
+});
